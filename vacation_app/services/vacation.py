0abe8b5290c7576a50265bd6179f2d97c50878f2
pr0d1gy <kirill.etc@gmail.com>
2015-07-07 16:02:14 +0300

    update vacations

---
+++ vacation_app/services/vacation.py
@@ -0,0 +1,70 @@
+from django.shortcuts import get_object_or_404
+
+from rest_framework import status
+
+from vacation_app.models import Vacation
+from vacation_app.models import Employee
+
+
+class VacationService(object):
+
+    errors = None
+    status = None
+
+    def create(self):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        self.errors = dict()
+        self.status = None
+
+        try:
+            vacation = Vacation.objects.get(pk=kwargs['pk'])
+        except Vacation.DoesNotExist:
+            self.errors['error'] = 'Vacation was not found.'
+            self.status = status.HTTP_404_NOT_FOUND
+
+            return False
+
+        if request.user.group_code == Employee.GMGER:
+            if vacation.state != [Vacation.VACATION_NEW]:
+                self.errors['error'] = 'Value can not be changed.'
+                self.status = status.HTTP_400_BAD_REQUEST
+
+                return False
+
+            if request.data['state'] not in [
+                    Vacation.VACATION_APPROVED_BY_MANAGER,
+                    Vacation.VACATION_REJECTED_BY_MANAGER
+                    ]:
+                self.errors['error'] = 'Value is not valid for manager.'
+                self.status = status.HTTP_400_BAD_REQUEST
+
+                return False
+
+        if request.user.group_code == Employee.GADMIN:
+            if vacation.state in [
+                    Vacation.VACATION_APPROVED_BY_ADMIN,
+                    Vacation.VACATION_REJECTED_BY_ADMIN
+                    ]:
+                self.errors['error'] = 'Value can not be changed.'
+                self.status = status.HTTP_400_BAD_REQUEST
+
+                return False
+
+            if request.data['state'] not in [
+                    Vacation.VACATION_APPROVED_BY_ADMIN,
+                    Vacation.VACATION_REJECTED_BY_ADMIN
+                    ]:
+                self.errors['error'] = 'Value is not valid for admin.'
+
+                return False
+
+        if request.user.group_code == Employee.GUSER:
+            if int(request.data['state']) != vacation.state:
+                self.errors['error'] = 'No permission to change state.'
+                self.status = status.HTTP_403_FORBIDDEN
+
+                return False
+
+        return True
--- vacation_app/views/vacation.py
+++ vacation_app/views/vacation.py
@@ -1,14 +1,14 @@
 from datetime import datetime
-from django.shortcuts import get_object_or_404
 
 from rest_framework import mixins
-from rest_framework import status
+
 from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 from rest_framework.permissions import IsAuthenticated
 
 from vacation_app.models import Employee, Vacation
 from vacation_app.serializers import VacationSerializer
+from vacation_app.services.vacation import VacationService
 
 
 class VacationViewSet(mixins.CreateModelMixin,
@@ -20,12 +20,15 @@ class VacationViewSet(mixins.CreateModelMixin,
     queryset = Vacation.objects.all()
     serializer_class = VacationSerializer
     permission_classes = (IsAuthenticated,)
+    service = VacationService()
 
     def get_queryset(self):
         if self.action == 'list':
 
             if 'id_user' in self.kwargs:
-                self.queryset = self.queryset.filter(user_id=self.kwargs['id_user'])
+                self.queryset = self.queryset.filter(
+                    user_id=self.kwargs['id_user']
+                )
 
             date_start = self.request.GET.get('start', None)
             date_end = self.request.GET.get('end', None)
@@ -33,12 +36,16 @@ class VacationViewSet(mixins.CreateModelMixin,
             try:
                 if date_start:
                     self.queryset = self.queryset.filter(
-                        date_start__gte=datetime.strptime(date_start, '%Y-%m-%d')
+                        date_start__gte=datetime.strptime(
+                            date_start, '%Y-%m-%d'
+                        )
                     )
 
                 if date_end:
                     self.queryset = self.queryset.exclude(
-                        date_end__gte=datetime.strptime(date_end, '%Y-%m-%d')
+                        date_end__gte=datetime.strptime(
+                            date_end, '%Y-%m-%d'
+                        )
                     )
             except ValueError:
                 pass
@@ -52,33 +59,12 @@ class VacationViewSet(mixins.CreateModelMixin,
         return self.queryset
 
     def update(self, request, *args, **kwargs):
-        vacation = get_object_or_404(Vacation, pk=kwargs['pk'])
+        if not self.service.update(request, *args, **kwargs):
+            return Response(
+                self.service.errors,
+                status=self.service.status
+            )
 
-        if request.user.group_code == Employee.GMGER:
-            if vacation.state != [Vacation.VACATION_NEW]:
-                return Response({'error': 'value can not be changed'},
-                                status=status.HTTP_400_BAD_REQUEST)
-
-            if request.data['state'] not in [
-                    Vacation.VACATION_APPROVED_BY_MANAGER,
-                    Vacation.VACATION_REJECTED_BY_MANAGER
-                    ]:
-                return Response({'error': 'value is not valid for manager'},
-                                status=status.HTTP_400_BAD_REQUEST)
-
-        if request.user.group_code == Employee.GADMIN:
-            if vacation.state in [
-                    Vacation.VACATION_APPROVED_BY_ADMIN,
-                    Vacation.VACATION_REJECTED_BY_ADMIN
-                    ]:
-                return Response({'error': 'value can not be changed'},
-                                status=status.HTTP_400_BAD_REQUEST)
-
-            if request.data['state'] not in [
-                    Vacation.VACATION_APPROVED_BY_ADMIN,
-                    Vacation.VACATION_REJECTED_BY_ADMIN
-                    ]:
-                return Response({'error': 'value is not valid for admin'},
-                                status=status.HTTP_400_BAD_REQUEST)
-
-        return super(VacationViewSet, self).update(request, *args, **kwargs)
+        return super(VacationViewSet, self).update(
+            request, *args, **kwargs
+        )
