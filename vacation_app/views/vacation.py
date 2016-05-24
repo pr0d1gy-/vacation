7b16fb0106c2cf5b1ad70557f12fe7cffc76767d
Kirill <kirill.etc@gmail.com>
2016-05-24 16:14:39 +0300

    add filter to vacations by month and year

--- vacation_app/views/vacation.py
+++ vacation_app/views/vacation.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from rest_framework import mixins
 
@@ -31,23 +31,65 @@ class VacationViewSet(mixins.CreateModelMixin,
 
             date_start = self.request.GET.get('start', None)
             date_end = self.request.GET.get('end', None)
+            month = self.request.GET.get('month', None)
+            year = self.request.GET.get('year', None)
 
-            try:
-                if date_start:
-                    self.queryset = self.queryset.filter(
-                        date_start__gte=datetime.strptime(
-                            date_start, '%Y-%m-%d'
+            if date_start and date_end and not month:
+                try:
+                    if date_start:
+                        self.queryset = self.queryset.filter(
+                            date_start__gte=datetime.strptime(
+                                date_start, '%Y-%m-%d'
+                            )
                         )
-                    )
 
-                if date_end:
-                    self.queryset = self.queryset.exclude(
-                        date_end__gte=datetime.strptime(
-                            date_end, '%Y-%m-%d'
+                    if date_end:
+                        self.queryset = self.queryset.exclude(
+                            date_end__lte=datetime.strptime(
+                                date_end, '%Y-%m-%d'
+                            )
                         )
-                    )
-            except ValueError:
-                pass
+                except ValueError:
+                    pass
+
+            else:
+                now = datetime.now()
+
+                try:
+                    month = int(month)
+                    if 1 > month or month > 12:
+                        raise ValueError
+                except (ValueError, TypeError):
+                    month = now.month
+
+                try:
+                    year = int(year)
+                except (ValueError, TypeError):
+                    year = now.year
+
+                start = (
+                    datetime.strptime('%s-%s' % (year, month), '%Y-%m') -
+                    timedelta(days=10)
+                )
+
+                if month == 12:
+                    month = 1
+                    year += 1
+
+                end = (
+                    datetime.strptime('%s-%s' % (year, month + 1), '%Y-%m') +
+                    timedelta(days=10)
+                )
+
+                self.queryset = self.queryset.filter(
+                    date_start__gte=start
+                )
+
+                self.queryset = self.queryset.filter(
+                    date_end__lte=end
+                )
+
+            self.queryset = self.queryset.order_by('date_start')
 
         if self.request.user.group_code not in [
             Employee.GADMIN,
