32ae076f27020fc73701e56eee3ab2cb3abd2d46
Kirill <kirill.etc@gmail.com>
2016-08-30 10:30:50 +0300

    test for exception in wsgi log

--- vacation_app/urls.py
+++ vacation_app/urls.py
@@ -6,6 +6,10 @@ from vacation_app.views import *
 from vacation_app.views import authorization
 
 
+def test_error(request):
+    return 2 / 0
+
+
 router = DefaultRouter()
 router.register(r'users', EmployeeViewSet)
 router.register(r'vacations', VacationViewSet)
@@ -14,5 +18,6 @@ router.register(r'mails', DeliveryViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-token-auth/', authorization.TokenAuthWithIdNameGroup.as_view()),
-    url(r'^i18n/', include('django.conf.urls.i18n'))
+    url(r'^i18n/', include('django.conf.urls.i18n')),
+    url(r'^test/$', test_error)
 ]
